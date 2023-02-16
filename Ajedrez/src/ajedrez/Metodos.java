5f82adac3734db1448c88f00a94eff0c4d4f9c62
LouCortes <lucianohcortes@gmail.com>
2023-02-16 10:12:06 +0100

    no message

--- Ajedrez/src/ajedrez/Metodos.java
+++ Ajedrez/src/ajedrez/Metodos.java
@@ -39,8 +39,7 @@ public class Metodos {
 
 		}
 		return num;
-	}
-	
+	}	
 
 
 }
--- Ajedrez/src/ajedrez/Principal.java
+++ Ajedrez/src/ajedrez/Principal.java
@@ -1,10 +1,24 @@
 package ajedrez;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		int row;
+		int col;
+		Scanner sc = new Scanner (System.in);
 		ChessBoard.initializeBoard();
 		ChessBoard.displayBoard();
+		
+		System.out.println("Selecciona una pieza");
+		String coordenada = sc.next();
+		
+		row = Integer.parseInt(coordenada.substring(1));
+		System.out.println(row);
+		
+		
+		
 	}
 
 }
