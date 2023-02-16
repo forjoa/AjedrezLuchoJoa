3ca6b3132e01c246f617167190c0fab5ade4c9dc
LouCortes <lucianohcortes@gmail.com>
2023-02-16 14:13:19 +0100

    Clases e intefaz

--- Ajedrez/src/ajedrez/Principal.java
+++ Ajedrez/src/ajedrez/Principal.java
@@ -11,22 +11,32 @@ public class Principal {
 		int DCol;
 		Scanner sc = new Scanner (System.in);
 		ChessBoard.initializeBoard();
-		ChessBoard.displayBoard();
+		String coordenada = "";
+		String destino = "";
+		String exit = "";
 		
-		System.out.println("Selecciona una pieza");
-		String coordenada = sc.next();
-		
-		SRow = Integer.parseInt(coordenada.substring(1));
-		SCol = Metodos.columna(coordenada.substring(0,1));
-		
-		System.out.println("Selecciona un Destino");
-		String destino = sc.next();
-		
-		DRow = Integer.parseInt(destino.substring(1));
-		DCol = Metodos.columna(destino.substring(0,1));
-		
-		ChessBoard.move(SCol, SRow, DCol, DRow);
-		ChessBoard.displayBoard();
+		while(!exit.equals("n")) {
+			
+			ChessBoard.displayBoard();
+			
+			System.out.println("Selecciona una pieza");
+			coordenada = sc.next();
+			
+			SRow = Integer.parseInt(coordenada.substring(1));
+			SCol = Metodos.columna(coordenada.substring(0,1));
+			
+			System.out.println("Selecciona un Destino");
+			destino = sc.next();
+			
+			DRow = Integer.parseInt(destino.substring(1));
+			DCol = Metodos.columna(destino.substring(0,1));
+			
+			ChessBoard.move(SCol, SRow, DCol, DRow);
+			ChessBoard.displayBoard();
+			
+			System.out.println("Presione n para salir");
+			exit = sc.next();
+		}
 		
 		
 		
