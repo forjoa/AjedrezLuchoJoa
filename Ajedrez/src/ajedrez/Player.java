562df4bf65a50b40d5c94ee20984db785db492f9
AL12025\USUARIO CCC <yaiza.escobarquintana2004@gmail.com>
2023-03-08 12:09:39 +0100

    Restriccion Jugadores Nuevos

--- Ajedrez/src/ajedrez/Player.java
+++ Ajedrez/src/ajedrez/Player.java
@@ -1,59 +1,73 @@
 package ajedrez;
 
 public class Player {
-	
+
 	private String name;
 	private boolean white;
-	
-	// constructores 
+
+	// constructores
 	public Player(String name, boolean white) {
 		super();
 		this.name = name;
 		this.white = white;
 	}
-	
+
 	public Player(String name) {
 		super();
 		this.name = name;
 	}
-	
+
 	public Player() {
-		
+
 	}
-	
+
 	/**
 	 * getters y setters
+	 * 
 	 * @return name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
 	 * getters y setters
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
 	 * getters y setters
 	 */
 	public void setWhite(boolean white) {
 		this.white = white;
 	}
-	
+
 	/**
-	 * method to know the color 
+	 * method to know the color
+	 * 
 	 * @return white
 	 */
 	public boolean isWhite() {
 		return white;
 	}
-	
+
 	/**
 	 * method for the users to know which color are they using
 	 */
-	public String toString(){
-		return "Nombre primer jugador: " + name + ", si su color es blanco: " + white;
+	public String toString() {
+		String mensaje = "";
+		if (white) {
+
+			mensaje = "Tu nombre es " + name + " y tu color es blanco.";
+		}
+		if (!white) {
+
+			mensaje = "Tu nombre es " + name + " y tu color es negro.";
+		}
+
+		return mensaje;
 	}
 
 }
